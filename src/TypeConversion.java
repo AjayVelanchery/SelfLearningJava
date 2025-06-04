dd1d34ef4bc262d14b7c6136f032ac6ff4cd552e
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-06-04 16:18:13 +0530

    My first two days java

---
+++ src/TypeConversion.java
@@ -0,0 +1,11 @@
+public class TypeConversion {
+
+    public static void main(String[] args) {
+        int i = (int)12.5f;
+        System.out.println(i);
+        float f=i;
+        System.out.println(f);
+        System.out.println("(int)12.5f==" + i);
+
+    }
+}
