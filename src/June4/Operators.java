044a376a243a25dfbfc7d508144679073a976ae9
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-06-05 11:53:25 +0530

    June 5

---
+++ src/June4/Operators.java
@@ -0,0 +1,13 @@
+package June4;
+
+public class Operators {
+    public static void main(String[] args) {
+        int a=190;
+        byte b=20;
+        double c = (3 * Math.pow(10, 80));
+        System.out.println((a+c));
+        System.out.println(a>b);
+        System.out.println(!(a>b&&b>a));
+
+    }
+}
