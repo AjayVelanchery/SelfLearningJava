e1132d7fa47baae85f6fc0dc1f32b6a2cf0f1db6
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-06-05 11:51:22 +0530

    June 5

---
+++ src/June4/TypeConversion.java
@@ -0,0 +1,18 @@
+package June4;
+
+public class TypeConversion {
+
+    public static void main(String[] args) {
+//        int i = (int)12.5f;
+//        System.out.println(i);
+//        float f=i;
+//        System.out.println(f);
+//        System.out.println("(int)12.5f==" + i);
+//
+
+        byte a=30;
+        byte b=20;
+        char c=(char) (a+b);
+        System.out.println(c);
+    }
+}
