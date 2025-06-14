321946f1f42371feadf76dc77957bfec49780839
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-06-14 12:42:36 +0530

    June 14

---
+++ src/June14/ThisAndSuper.java
@@ -0,0 +1,35 @@
+package June14;
+
+
+
+ class A{
+
+    public A(){
+        System.out.println("Parent default");
+    }
+
+    public A(int n){
+        System.out.println("Parent value");
+    }
+}
+
+class B extends A{
+
+     public B(){
+         super(5);
+         System.out.println("child default");
+     }
+     public B(int n){
+
+         System.out.println("child value");
+     }
+
+
+}
+public class ThisAndSuper {
+    public static void main(String[] args) {
+
+        B ob=new B();
+
+    }
+}
