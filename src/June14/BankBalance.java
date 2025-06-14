62596f0e3d859ad295a04a1a2bf5e3b48436bb64
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-06-14 20:20:20 +0530

    June 14

---
+++ src/June14/BankBalance.java
@@ -0,0 +1,7 @@
+package June14;
+//- Create a class BankAccount with methods deposit, withdraw, displayBalance.
+//        - Use constructor to initialize account holder and balance.
+
+
+public class BankBalance {
+}
