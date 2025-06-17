ae4e04a9fe17e892bc30f0824afe44fe1a20c8a6
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-06-17 10:17:42 +0530

    June 17

---
+++ src/June17/StaticLearning.java
@@ -0,0 +1,15 @@
+package June17;
+
+
+class Sample{
+    static {
+        System.out.println("Hai just testing");
+    }
+}
+
+public class StaticLearning {
+    public static void main(String[] args) {
+        Sample s = new Sample();
+
+    }
+}
