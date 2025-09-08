3b0078f345456b103bb928c70e611e0019b3783e
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-09-08 19:32:49 +0530

    binary search

---
+++ src/KhunalDSA/OrderAgnosticBS.java
@@ -0,0 +1,51 @@
+package KhunalDSA;
+
+public class OrderAgnosticBS {
+    public static void main(String[] args) {
+
+int []arr={-118,-12,-4,0,2,3,4,14,16};
+int target=16;
+
+        System.out.println(orderAgnosticBS(arr,target));
+    }
+
+static int orderAgnosticBS(int []arr,int target){
+
+  int start=0;
+        int end=arr.length-1;
+
+    boolean isAsc=arr[start]<arr[end];
+
+        while(start<=end){
+
+            int mid=start+(end-start)/2;
+
+
+            if(isAsc){
+            if(target<arr[mid]){
+
+                end=mid-1;
+            } else if (target>arr[mid]) {
+                start=mid+1;
+
+            }
+            else{
+                return mid;
+            }}
+
+            else{
+
+                if(target<arr[mid]){
+
+                    end=mid+1;
+                } else if (target>arr[mid]) {
+                    start=mid-1;
+
+                }
+                else{
+                    return mid;
+                }}
+            }
+
+return -1;
+}}
