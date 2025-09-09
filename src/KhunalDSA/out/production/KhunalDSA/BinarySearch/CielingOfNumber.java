02d74f86b7ceae35aad34e36040dc5d814b839be
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-09-09 18:37:59 +0530

    binary search floor number

--- src/KhunalDSA/out/production/KhunalDSA/BinarySearch/CielingOfNumber.java
+++ src/KhunalDSA/out/production/KhunalDSA/BinarySearch/CielingOfNumber.java
@@ -7,6 +7,7 @@ public class CielingOfNumber {
 int []arr={2,3,5,9,14,16,18};
 int target=17;
         System.out.println(cielingNumber(arr,target));
+        System.out.println(floorNumber(arr,target));
     }
 
     static int cielingNumber(int []arr,int target){
@@ -33,5 +34,31 @@ int target=17;
 
         }
         return arr[left];
+    }
+
+    static int floorNumber(int []arr,int target){
+
+
+        int left=0;
+        int right=arr.length;
+
+        while(left<=right){
+
+
+            int mid=left+(right-left)/2;
+
+            if(arr[mid]==target){
+                return mid;
+            }
+
+            if(arr[mid]<target){
+                left=mid+1;
+            }
+            else {
+                right=mid-1;
+            }
+
+        }
+        return arr[right];
     }
 }
