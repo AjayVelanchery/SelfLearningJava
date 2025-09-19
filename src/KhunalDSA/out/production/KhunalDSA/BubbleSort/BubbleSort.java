ecb5c334709dd0ec9c31ebab3935352ea3314f02
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-09-19 12:13:45 +0530

    Bubble Sort

---
+++ src/KhunalDSA/out/production/KhunalDSA/BubbleSort/BubbleSort.java
@@ -0,0 +1,31 @@
+package KhunalDSA.out.production.KhunalDSA.BubbleSort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+
+    public static void main(String[] args) {
+        int arr[]={3,4,2,1,3,5};
+        int n=arr.length;
+
+        System.out.println(Arrays.toString(bubbleSort(arr,n)));
+
+    }
+
+    static int[] bubbleSort(int []arr,int n){
+
+        for(int i=0;i<n;i++){
+
+            for(int j=1;j<n-i;j++){
+
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
