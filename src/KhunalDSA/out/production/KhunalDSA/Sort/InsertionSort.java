ba035f5e7dd6f3887ad29a98d8f167c8cff9a6c8
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-09-20 16:07:03 +0530

    insertion sort

--- src/KhunalDSA/out/production/KhunalDSA/Sort/InsertionSort.java
+++ src/KhunalDSA/out/production/KhunalDSA/Sort/InsertionSort.java
@@ -1,18 +1,24 @@
 package KhunalDSA.out.production.KhunalDSA.Sort;
 
+import java.util.Arrays;
+
 public class InsertionSort {
 
 
     public static void main(String[] args) {
 int []arr={4,5,1,2,3};
+int []arr1={0,-23,56,18};
 insertionSort(arr);
+        System.out.println(Arrays.toString((arr)));
+insertionSort(arr1);
+        System.out.println(Arrays.toString(arr1));
     }
 
     static void insertionSort(int []arr){
 
         for(int i=0;i<arr.length-1;i++){
 
-            for(int j=i+1;j>=0;j--){
+            for(int j=i+1;j>0;j--){
 
 if(arr[j]<arr[j-1]){
     swap(arr,j,j-1);
