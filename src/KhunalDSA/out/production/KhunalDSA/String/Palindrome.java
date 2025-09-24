4c3dbdc1405329733073453fa3a66dd88c2b39c2
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-09-24 09:46:49 +0530

    string palindrome

---
+++ src/KhunalDSA/out/production/KhunalDSA/String/Palindrome.java
@@ -0,0 +1,23 @@
+package KhunalDSA.out.production.KhunalDSA.String;
+
+public class Palindrome {
+    public static void main(String[] args) {
+
+        String s="malayalam";
+        System.out.println(isPalindrome(s));
+
+    }
+
+    static boolean isPalindrome(String s){
+        int i=0;
+        int j=s.length()-1;
+while(i<j)            {
+
+    if(s.charAt(i)!=s.charAt(j)){
+        return false;
+    }i++;
+    j--;
+        }
+return true;
+    }
+}
