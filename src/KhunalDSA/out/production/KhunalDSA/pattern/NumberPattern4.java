974fa4982386df2a44fd0954e315378c8e7a0704
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-09-24 13:54:14 +0530

    patterns 4 and 5

---
+++ src/KhunalDSA/out/production/KhunalDSA/pattern/NumberPattern4.java
@@ -0,0 +1,16 @@
+package KhunalDSA.out.production.KhunalDSA.pattern;
+
+public class NumberPattern4 {
+    public static void main(String[] args) {
+
+
+        int n = 5;
+        for (int i = 1; i <=n; i++) {
+            int k=0;
+            for (int j = 1; j <= i; j++) {
+                k++;
+                System.out.print(k+" ");
+            }
+            System.out.println("");
+        }
+    }}
