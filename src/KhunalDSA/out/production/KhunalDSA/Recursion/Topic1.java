941518c9b5e4e16d49a02f3acc794b3e50ecf776
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-09-25 10:05:17 +0530

    recursion starting

---
+++ src/KhunalDSA/out/production/KhunalDSA/Recursion/Topic1.java
@@ -0,0 +1,14 @@
+package KhunalDSA.out.production.KhunalDSA.Recursion;
+
+public class Topic1 {
+
+
+    public static void main(String[] args) {
+
+message();
+    }
+
+    static void message(){
+        System.out.println("Hello world");
+    }
+}
