9f11192d07d13b6af1f95a22b698fdc04bd6e0c1
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-09-26 15:56:11 +0530

    binary fibo

---
+++ src/KhunalDSA/out/production/KhunalDSA/Recursion/BinarySearch.java
@@ -0,0 +1,32 @@
+package KhunalDSA.out.production.KhunalDSA.Recursion;
+
+public class BinarySearch {
+
+
+    public static void main(String[] args) {
+        int []arr={1,3,4,5,6,8};
+        int target=5;
+        System.out.println(binarySearch(arr,target,0,arr.length-1));
+
+    }
+
+    static int binarySearch(int []arr,int target,int start,int end)
+
+    {
+        if(start>end){
+            return -1;
+        }
+
+        int mid=start+(end-start)/2;
+
+        if(arr[mid]==target){
+            return mid;
+        }
+        if(target<arr[mid]){
+            return binarySearch(arr,target,start,mid-1);
+        } else  {
+            return binarySearch(arr,target,mid+1,end);
+        }
+    }
+
+}
