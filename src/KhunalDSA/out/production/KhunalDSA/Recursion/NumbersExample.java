8b22e415ab98626d9f70a578254c19e61d6caa9f
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-09-27 13:52:50 +0530

    checking on recursion

--- src/KhunalDSA/out/production/KhunalDSA/Recursion/NumbersExample.java
+++ src/KhunalDSA/out/production/KhunalDSA/Recursion/NumbersExample.java
@@ -6,12 +6,12 @@ public class NumbersExample {
 
         print1(1);
     }
-//    static void print1(int n){
-//        System.out.println(n);
-//        print2(2);
-//    }
-//    static void print2(int n){
-//        System.out.println(n);
+    static void print1(int n){
+        System.out.println(n);
+        print2(2);
+    }
+    static void print2(int n){
+        System.out.println(n);
 //        print3(3);
 //    }
 //    static void print3(int n){
