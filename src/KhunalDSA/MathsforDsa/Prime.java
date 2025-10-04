55488ab6d54788c9361d8a22fd15f41a36b1fb3d
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-10-04 12:37:44 +0530

    PrimeNos

--- src/KhunalDSA/MathsforDsa/Prime.java
+++ src/KhunalDSA/MathsforDsa/Prime.java
@@ -5,6 +5,9 @@ public class Prime {
 
     public static void main(String[] args) {
         System.out.println(isPrime(31));
+        int n=40;
+        boolean [] primes=new boolean[n+1];
+        sieve(n,primes);
 
     }
     static boolean isPrime(int n){
@@ -21,6 +24,26 @@ public class Prime {
      }
 
      return true;
+
+    }
+
+    static void sieve(int n,boolean[]prime){
+
+        for(int i=2;i*i<n;i++){
+
+            if(!prime[i]){
+                for(int j=2*i;j<=n;j+=i){
+                    prime[j]=true;
+
+                }
+            }
+        }
+        for(int i=2;i<=n;i++){
+
+        if(!prime[i]){
+            System.out.println(i+ " ");
+        }
+        }
 
     }
 }
