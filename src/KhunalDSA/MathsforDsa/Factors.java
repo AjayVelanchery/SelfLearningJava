5d5ee03eaa187bf61fccafb96f00361ff174f846
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-10-06 10:10:37 +0530

    factors2

--- src/KhunalDSA/MathsforDsa/Factors.java
+++ src/KhunalDSA/MathsforDsa/Factors.java
@@ -4,7 +4,7 @@ public class Factors {
 
     public static void main(String[] args) {
 
-        factors1(20);
+        factors2(20);
     }
 
     static void factors1(int n){
@@ -15,6 +15,19 @@ public class Factors {
             }
         }
     }
+static void factors2(int n){
+        for(int i=1;i<=Math.sqrt(n);i++){
+
+            if(n%i==0){
+                if(n/i==i){
+                    System.out.print(i);
+                }else {
+                    System.out.print(i+ "  " +n/i+" ");
+                }
+            }
+        }
+}
+
 
 
 }
