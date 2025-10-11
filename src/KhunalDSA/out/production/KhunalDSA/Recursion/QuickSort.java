5e32b5bccf3fbd0989d7349fcb95be217d5a7568
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-10-11 12:47:52 +0530

    quick sort

--- src/KhunalDSA/out/production/KhunalDSA/Recursion/QuickSort.java
+++ src/KhunalDSA/out/production/KhunalDSA/Recursion/QuickSort.java
@@ -1,4 +1,43 @@
 package KhunalDSA.out.production.KhunalDSA.Recursion;
 
+import java.util.Arrays;
+
 public class QuickSort {
+
+    public static void main(String[] args) {
+
+        int []arr={5,4,3,2,1};
+       sort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void sort(int []arr,int low,int high){
+if(low>=high){
+    return;
+}
+        int start=low;
+        int end=high;
+        int pivot=arr[start+(end-start)/2];
+        while(start<=end){
+
+            while(arr[start]<pivot){
+
+                start++;
+            }
+            while (arr[end]>pivot){
+                end--;
+            }
+
+            if(start<=end){
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+
+                start++;
+                end--;
+            }
+
+        }
+        sort(arr,low,end);
+        sort(arr,start,high);
+    }
 }
