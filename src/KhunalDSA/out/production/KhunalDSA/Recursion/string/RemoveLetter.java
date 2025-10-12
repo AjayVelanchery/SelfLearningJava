58a517af5b613a56eec06f097b8478c7a530a539
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-10-12 19:31:35 +0530

    remove a

---
+++ src/KhunalDSA/out/production/KhunalDSA/Recursion/string/RemoveLetter.java
@@ -0,0 +1,29 @@
+package KhunalDSA.out.production.KhunalDSA.Recursion.string;
+
+public class RemoveLetter {
+
+    public static void main(String[] args) {
+        String st="chaad";
+
+        skip("",st);
+
+    }
+
+     static void skip(String p,String up){
+
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+
+        }
+        char ch=up.charAt(0);
+        if(ch=='a'){
+            skip(p,up.substring(1));
+        }
+        else {
+            skip(p+ch,up.substring(1));
+        }
+     }
+
+
+}
