93a2196cc02b2ef6ba53ce34013184fe5c6367ad
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-10-18 10:17:49 +0530

    backtracking

--- src/KhunalDSA/out/production/KhunalDSA/Recursion/backtracking/Maze.java
+++ src/KhunalDSA/out/production/KhunalDSA/Recursion/backtracking/Maze.java
@@ -4,6 +4,7 @@ public class Maze {
 
     public static void main(String[] args) {
         System.out.println(count(3,3));
+        path(3,3,"");
 
     }
 
@@ -16,4 +17,19 @@ static int count(int r,int c){
     int right=count(r,c-1);
     return left+right;
 }
+static void path(int r,int c,String p){
+
+        if(r==1 && c==1){
+            System.out.println(p);
+            return;
+        }
+
+        if(r>1){
+            path(r-1,c,p+'D');
+        }
+        if(c>1){
+            path(r,c-1,p+'R');
+        }
+}
+
 }
