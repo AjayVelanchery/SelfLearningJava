a982431de1892cf9d0cebc7fec7115ebf7844b9e
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-11-06 23:15:59 +0530

    study flux

---
+++ src/october/FluxLearning.java
@@ -0,0 +1,28 @@
+package october;
+
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FluxLearning {
+
+    public static void main(String[] args) {
+
+        List<String> fruits= Arrays.asList("Apple","Banana","Cherry");
+
+        Flux<String > fruitsFlux=Flux.fromIterable(fruits);
+
+        Flux<String> processedflux=fruitsFlux
+                .filter(f->f.length()>5)
+                .map(String::toUpperCase);
+
+    processedflux.subscribe(
+            data-> System.out.println("Fruits: "+data),
+            error-> System.err.println("Error"+error.getMessage()),
+            ()-> System.out.println("flux stream finished")
+
+    );
+    }
+
+}
