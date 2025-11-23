093456c7f8634b4264ca264264094b269562adad
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-11-23 18:46:17 +0530

    linked list

---
+++ src/KhunalDSA/linkedlist/Main.java
@@ -0,0 +1,41 @@
+package KhunalDSA.linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+//
+//
+//LL list=new LL();
+//
+//list.insertFirst(3);
+//list.insertFirst(2);
+//list.insertFirst(1);
+//list.insertLast(99);
+//
+//list.insert(100,2);
+//        System.out.println(list.deleteFirst());
+//
+//list.display();
+
+
+//        DLL list=new DLL();
+//
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(1);
+//        list.insertLast(4);
+//
+//
+//list.insert(8,33);
+//        list.display();
+
+        CLL list=new CLL();
+list.insert(23);
+        list.insert(32);
+        list.insert(23);list.insert(34);
+
+list.delete(23);
+        list.display();
+
+
+}}
